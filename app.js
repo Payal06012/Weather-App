404bbde44e780bdb11b546bd28707db9af726141
payal <payalpatel3727@gmail.com>
2024-10-20 01:32:27 +0530

    Added input field for entering city name and basic info of weather

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('search-btn').addEventListener('click', () => {
+    const city = document.getElementById('city-input').value;
+    console.log('City entered:', city);
+    
+  });
+  
+    
